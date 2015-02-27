2d88a659813868286a493eaa71acb289604ba8b4
Levi <levi@byimplication.com>
2015-02-28 01:55:50 +0800

    simplifies collection library

--- resources/public/js/out/meetapp/lib/collections.cljs
+++ resources/public/js/out/meetapp/lib/collections.cljs
@@ -1,13 +1,10 @@
 (ns meetapp.lib.collections)
 
-(defn without [n coll]
-  (let [v (vec coll)]
-    (into (subvec v 0 n) (subvec v (inc n)))))
+(defn without [n vctr]
+  (into (subvec vctr 0 n) (subvec vctr (inc n))))
 
-(defn insert [coll n value]
-  (let [v (vec coll)]
-    (vec (concat (subvec v 0 n) [value] (subvec v n)))))
+(defn insert [vctr n value]
+  (vec (concat (subvec vctr 0 n) [value] (subvec vctr n))))
 
-(defn reposition [coll from to]
-  (let [v (vec coll)]
-    (insert (without from v) to (v from))))
+(defn reposition [vctr from to]
+  (insert (without from vctr) to (vctr from)))
--- resources/public/js/out/meetapp/lib/collections.js
+++ resources/public/js/out/meetapp/lib/collections.js
@@ -1,17 +1,14 @@
 // Compiled by ClojureScript 0.0-2760 {}
 goog.provide('meetapp.lib.collections');
 goog.require('cljs.core');
-meetapp.lib.collections.without = (function without(n,coll){
-var v = cljs.core.vec.call(null,coll);
-return cljs.core.into.call(null,cljs.core.subvec.call(null,v,(0),n),cljs.core.subvec.call(null,v,(n + (1))));
+meetapp.lib.collections.without = (function without(n,vctr){
+return cljs.core.into.call(null,cljs.core.subvec.call(null,vctr,(0),n),cljs.core.subvec.call(null,vctr,(n + (1))));
 });
-meetapp.lib.collections.insert = (function insert(coll,n,value){
-var v = cljs.core.vec.call(null,coll);
-return cljs.core.vec.call(null,cljs.core.concat.call(null,cljs.core.subvec.call(null,v,(0),n),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [value], null),cljs.core.subvec.call(null,v,n)));
+meetapp.lib.collections.insert = (function insert(vctr,n,value){
+return cljs.core.vec.call(null,cljs.core.concat.call(null,cljs.core.subvec.call(null,vctr,(0),n),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [value], null),cljs.core.subvec.call(null,vctr,n)));
 });
-meetapp.lib.collections.reposition = (function reposition(coll,from,to){
-var v = cljs.core.vec.call(null,coll);
-return meetapp.lib.collections.insert.call(null,meetapp.lib.collections.without.call(null,from,v),to,v.call(null,from));
+meetapp.lib.collections.reposition = (function reposition(vctr,from,to){
+return meetapp.lib.collections.insert.call(null,meetapp.lib.collections.without.call(null,from,vctr),to,vctr.call(null,from));
 });
 
 //# sourceMappingURL=collections.js.map
